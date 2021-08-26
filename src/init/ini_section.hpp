ab5a66a7473de4d652baadeefc95daa1b568c5a8
Albin Johansson <albinoscar99@hotmail.com>
2021-08-26 13:36:38 +0200

    Add ini_section.hpp

---
+++ src/init/ini_section.hpp
@@ -0,0 +1,162 @@
+#ifndef INIT_INI_SECTION_HPP
+#define INIT_INI_SECTION_HPP
+
+#include <functional>   // less
+#include <map>          // map
+#include <ostream>      // basic_ostream
+#include <stdexcept>    // out_of_range
+#include <string>       // basic_string, char_traits
+#include <string_view>  // basic_string_view
+
+#include "common.hpp"
+#include "ini_value.hpp"
+
+namespace init {
+
+template <typename Char>
+struct ini_format final
+{
+  using value_type = Char;
+
+  value_type section_start = '[';  ///< Token introducing a section name.
+  value_type section_end = ']';    ///< Token that ends a section name.
+  value_type assign = '=';         ///< Assignment operator token.
+  value_type comment = ';';        ///< Line comment token.
+};
+
+template <typename Char>
+class basic_ini_section final
+{
+ public:
+  using char_type = Char;
+  using elem_type = basic_ini_value<char_type>;
+  using format_type = ini_format<char_type>;
+
+  using string_type = std::basic_string<char_type>;
+  using string_view_type = std::basic_string_view<char_type>;
+  using ostream_type = std::basic_ostream<char_type, std::char_traits<char_type>>;
+
+  using storage_type = std::map<string_type, elem_type, std::less<>>;
+  using value_type = typename storage_type::value_type;
+  using iterator = typename storage_type::iterator;
+  using const_iterator = typename storage_type::const_iterator;
+  using size_type = usize;
+
+  basic_ini_section() = default;
+
+  basic_ini_section(const basic_ini_section&) = default;
+  basic_ini_section(basic_ini_section&&) noexcept = default;
+
+  basic_ini_section& operator=(const basic_ini_section&) = default;
+  basic_ini_section& operator=(basic_ini_section&&) noexcept = default;
+
+  void dump(ostream_type& stream, const format_type& format) const
+  {
+    for (const auto& [key, value] : m_entries)
+    {
+      stream << key << format.assign << value << '\n';
+    }
+
+    stream << '\n';
+  }
+
+  auto erase(const string_view_type element) -> bool
+  {
+    if (const auto it = m_entries.find(element); it != m_entries.end())
+    {
+      m_entries.erase(it);
+      return true;
+    }
+    else
+    {
+      return false;
+    }
+  }
+
+  auto get_or_emplace(const string_view_type element) -> elem_type&
+  {
+    if (const auto it = m_entries.find(element); it != m_entries.end())
+    {
+      return it->second;
+    }
+    else
+    {
+      return m_entries[string_type{element}];
+    }
+  }
+
+  /// \copydoc get_or_emplace()
+  auto operator[](const string_view_type element) -> elem_type&
+  {
+    return get_or_emplace(element);
+  }
+
+  [[nodiscard]] auto at(const string_view_type element) -> elem_type&
+  {
+    if (const auto it = m_entries.find(element); it != m_entries.end())
+    {
+      return it->second;
+    }
+    else
+    {
+      throw std::out_of_range{"basic_ini_section::at(): element does not exist!"};
+    }
+  }
+
+  [[nodiscard]] auto at(const string_view_type element) const -> const elem_type&
+  {
+    if (const auto it = m_entries.find(element); it != m_entries.end())
+    {
+      return it->second;
+    }
+    else
+    {
+      throw std::out_of_range{"basic_ini_section::at(): element does not exist!"};
+    }
+  }
+
+  [[nodiscard]] auto contains(const string_view_type element) const -> bool
+  {
+    return m_entries.find(element) != m_entries.end();
+  }
+
+  [[nodiscard]] auto size() const noexcept -> size_type
+  {
+    return m_entries.size();
+  }
+
+  [[nodiscard]] auto empty() const noexcept -> bool
+  {
+    return m_entries.empty();
+  }
+
+  [[nodiscard]] auto begin() noexcept -> iterator
+  {
+    return m_entries.begin();
+  }
+
+  [[nodiscard]] auto begin() const noexcept -> const_iterator
+  {
+    return m_entries.begin();
+  }
+
+  [[nodiscard]] auto end() noexcept -> iterator
+  {
+    return m_entries.end();
+  }
+
+  [[nodiscard]] auto end() const noexcept -> const_iterator
+  {
+    return m_entries.end();
+  }
+
+ private:
+  storage_type m_entries;
+};
+
+using ini_section = basic_ini_section<char>;
+using wini_section = basic_ini_section<wchar_t>;
+
+}  // namespace init
+
+#endif  // INIT_INI_SECTION_HPP
---
+++ test/unit-tests/init/ini_section_test.cpp
@@ -0,0 +1,140 @@
+#include "ini_section.hpp"
+
+#include <gtest/gtest.h>
+
+#include <algorithm>  // for_each
+#include <iostream>   // cout
+#include <string>     // to_string
+
+TEST(IniSection, Defaults)
+{
+  const init::ini_section section;
+  ASSERT_EQ(0, section.size());
+  ASSERT_TRUE(section.empty());
+}
+
+TEST(IniSection, Dump)
+{
+  init::ini_section section;
+
+  section["foo"] = "bar";
+  section["boo"] = 42;
+  section["doo"] = 12.3;
+  section["zoo"] = true;
+  section["noo"] = 27u;
+
+  std::cout << "Section with standard format: \n";
+  section.dump(std::cout, init::ini_section::format_type{});
+}
+
+TEST(IniSection, Erase)
+{
+  init::ini_section section;
+  ASSERT_FALSE(section.erase("foo"));
+
+  section["foo"] = "abc";
+  section["bar"] = 42;
+  ASSERT_EQ(2, section.size());
+  ASSERT_TRUE(section.contains("foo"));
+  ASSERT_TRUE(section.contains("bar"));
+
+  ASSERT_TRUE(section.erase("foo"));
+  ASSERT_FALSE(section.erase("foo"));
+  ASSERT_FALSE(section.contains("foo"));
+  ASSERT_EQ(1, section.size());
+
+  ASSERT_FALSE(section.erase("Bar"));
+  ASSERT_TRUE(section.erase("bar"));
+  ASSERT_TRUE(section.empty());
+
+  const std::string str = "bar";
+  ASSERT_FALSE(section.erase(str));
+}
+
+TEST(IniSection, GetOrEmplace)
+{
+  init::ini_section section;
+
+  auto& a = section.get_or_emplace("foo");
+  ASSERT_EQ(1, section.size());
+  ASSERT_TRUE(section.contains("foo"));
+
+  auto& b = section.get_or_emplace("foo");
+  ASSERT_EQ(1, section.size());
+  ASSERT_EQ(&a, &b);
+
+  section.get_or_emplace("bar") = 42u;
+  ASSERT_EQ(2, section.size());
+  ASSERT_EQ(42u, section.at("bar"));
+}
+
+TEST(IniSection, SubscriptOperator)
+{
+  init::ini_section section;
+
+  auto& a = section["foo"];
+  ASSERT_EQ(1, section.size());
+  ASSERT_TRUE(section.contains("foo"));
+
+  auto& b = section["foo"];
+  ASSERT_EQ(1, section.size());
+  ASSERT_EQ(&a, &b);
+
+  section["bar"] = "hello";
+  ASSERT_EQ(2, section.size());
+  ASSERT_EQ("hello", section.at("bar"));
+}
+
+TEST(IniSection, At)
+{
+  init::ini_section section;
+  const auto& ref = section;
+
+  ASSERT_THROW(section.at("foo"), std::out_of_range);
+  ASSERT_THROW(ref.at("foo"), std::out_of_range);
+
+  section["foo"] = "bar";
+  ASSERT_EQ("bar", section.at("foo"));
+  ASSERT_EQ("bar", ref.at("foo"));
+}
+
+TEST(IniSection, Contains)
+{
+  init::ini_section section;
+  ASSERT_FALSE(section.contains("foo"));
+
+  section["foo"] = 123;
+  ASSERT_TRUE(section.contains("foo"));
+
+  section.erase("foo");
+  ASSERT_FALSE(section.contains("foo"));
+}
+
+TEST(IniSection, Iteration)
+{
+  init::ini_section section;
+
+  section["0"] = 0;
+  section["1"] = 1;
+  section["2"] = 2;
+
+  int index = 0;
+  for (const auto& [name, value] : section)
+  {
+    ASSERT_EQ(index, section.at(std::to_string(index)));
+    ++index;
+  }
+
+  using pair_type = std::pair<const std::string, init::ini_value>;
+  std::ranges::for_each(section, [](pair_type& elem) {
+    auto& value = elem.second;
+    value = value.as<int>() + 1;
+  });
+
+  index = 0;
+  for (const auto& [name, value] : section)
+  {
+    ASSERT_EQ(index + 1, section.at(std::to_string(index)));
+    ++index;
+  }
+}
