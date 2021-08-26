13fce56949d99d7acc1ef215e577006d6fe6496e
Albin Johansson <albinoscar99@hotmail.com>
2021-08-26 12:58:15 +0200

    Add ini_value.hpp

---
+++ src/init/ini_value.hpp
@@ -0,0 +1,250 @@
+#ifndef INIT_INI_VALUE_HPP
+#define INIT_INI_VALUE_HPP
+
+#include <concepts>  // integral, floating_point, constructible_from, same_as, ...
+#include <ostream>   // ostream
+#include <string>    // basic_string, to_string
+#include <utility>   // move
+#include <variant>   // variant, get, get_if, holds_alternative
+
+#include "common.hpp"
+
+namespace init {
+
+template <typename T, typename Char>
+concept is_ini_value = std::constructible_from<std::basic_string<Char>, T> ||
+    std::integral<T> || std::floating_point<T>;
+
+template <typename Char>
+class basic_ini_value final
+{
+ public:
+  using char_type = Char;
+  using int_type = int64;
+  using uint_type = uint64;
+  using float_type = double;
+  using string_type = std::basic_string<char_type>;
+  using value_type = std::variant<string_type, bool, int_type, uint_type, float_type>;
+
+  basic_ini_value() = default;
+
+  basic_ini_value(const basic_ini_value&) = default;
+  basic_ini_value(basic_ini_value&&) noexcept = default;
+
+  basic_ini_value& operator=(const basic_ini_value&) = default;
+  basic_ini_value& operator=(basic_ini_value&&) noexcept = default;
+
+  template <is_ini_value<char_type> T>
+  /*implicit*/ basic_ini_value(T value)  // NOLINT
+  {
+    assign(std::move(value));
+  }
+
+  template <is_ini_value<char_type> T>
+  auto operator=(T value) -> basic_ini_value&
+  {
+    assign(std::move(value));
+    return *this;
+  }
+
+  void dump(std::ostream& stream) const
+  {
+    if (const auto* str = try_as_string())
+    {
+      stream << *str;
+    }
+    else if (const auto* i = try_as_int())
+    {
+      stream << std::to_string(*i);
+    }
+    else if (const auto* u = try_as_uint())
+    {
+      stream << std::to_string(*u) << 'u';
+    }
+    else if (const auto* f = try_as_float())
+    {
+      stream << std::to_string(*f);
+    }
+    else if (const auto* b = try_as_bool())
+    {
+      stream << ((*b) ? "true" : "false");
+    }
+  }
+
+  template <is_ini_value<char_type> T>
+  [[nodiscard]] auto as() const -> T
+  {
+    if constexpr (std::same_as<T, bool>)
+    {
+      return std::get<bool>(m_value);
+    }
+    else if constexpr (std::signed_integral<T>)
+    {
+      return static_cast<T>(std::get<int_type>(m_value));
+    }
+    else if constexpr (std::unsigned_integral<T>)
+    {
+      return static_cast<T>(std::get<uint_type>(m_value));
+    }
+    else if constexpr (std::floating_point<T>)
+    {
+      return static_cast<T>(std::get<float_type>(m_value));
+    }
+    else
+    {
+      return std::get<string_type>(m_value);
+    }
+  }
+
+  void get_to(string_type& value) const
+  {
+    value = std::get<string_type>(m_value);
+  }
+
+  void get_to(bool& value) const
+  {
+    value = std::get<bool>(m_value);
+  }
+
+  void get_to(int8& value) const
+  {
+    value = static_cast<int8>(std::get<int_type>(m_value));
+  }
+
+  void get_to(int16& value) const
+  {
+    value = static_cast<int16>(std::get<int_type>(m_value));
+  }
+
+  void get_to(int32& value) const
+  {
+    value = static_cast<int32>(std::get<int_type>(m_value));
+  }
+
+  void get_to(int64& value) const
+  {
+    value = static_cast<int64>(std::get<int_type>(m_value));
+  }
+
+  void get_to(uint8& value) const
+  {
+    value = static_cast<uint8>(std::get<uint_type>(m_value));
+  }
+
+  void get_to(uint16& value) const
+  {
+    value = static_cast<uint16>(std::get<uint_type>(m_value));
+  }
+
+  void get_to(uint32& value) const
+  {
+    value = static_cast<uint32>(std::get<uint_type>(m_value));
+  }
+
+  void get_to(uint64& value) const
+  {
+    value = static_cast<uint64>(std::get<uint_type>(m_value));
+  }
+
+  void get_to(float& value) const
+  {
+    value = static_cast<float>(std::get<float_type>(m_value));
+  }
+
+  void get_to(double& value) const
+  {
+    value = static_cast<double>(std::get<float_type>(m_value));
+  }
+
+  void get_to(long double& value) const
+  {
+    value = static_cast<long double>(std::get<float_type>(m_value));
+  }
+
+  [[nodiscard]] auto try_as_string() const noexcept -> const string_type*
+  {
+    return std::get_if<string_type>(&m_value);
+  }
+
+  [[nodiscard]] auto try_as_bool() const noexcept -> const bool*
+  {
+    return std::get_if<bool>(&m_value);
+  }
+
+  [[nodiscard]] auto try_as_int() const noexcept -> const int_type*
+  {
+    return std::get_if<int_type>(&m_value);
+  }
+
+  [[nodiscard]] auto try_as_uint() const noexcept -> const uint_type*
+  {
+    return std::get_if<uint_type>(&m_value);
+  }
+
+  [[nodiscard]] auto try_as_float() const noexcept -> const float_type*
+  {
+    return std::get_if<float_type>(&m_value);
+  }
+
+  [[nodiscard]] auto is_string() const noexcept -> bool
+  {
+    return std::holds_alternative<string_type>(m_value);
+  }
+
+  [[nodiscard]] auto is_bool() const noexcept -> bool
+  {
+    return std::holds_alternative<bool>(m_value);
+  }
+
+  [[nodiscard]] auto is_int() const noexcept -> bool
+  {
+    return std::holds_alternative<int_type>(m_value);
+  }
+
+  [[nodiscard]] auto is_uint() const noexcept -> bool
+  {
+    return std::holds_alternative<uint_type>(m_value);
+  }
+
+  [[nodiscard]] auto is_float() const noexcept -> bool
+  {
+    return std::holds_alternative<float_type>(m_value);
+  }
+
+  [[nodiscard]] bool operator==(const basic_ini_value&) const = default;
+
+ private:
+  value_type m_value;
+
+  template <is_ini_value<char_type> T>
+  void assign(T value)
+  {
+    if constexpr (std::same_as<T, bool>)
+    {
+      m_value.template emplace<bool>(std::move(value));
+    }
+    else if constexpr (std::signed_integral<T>)
+    {
+      m_value.template emplace<int_type>(std::move(value));
+    }
+    else if constexpr (std::unsigned_integral<T>)
+    {
+      m_value.template emplace<uint_type>(std::move(value));
+    }
+    else if constexpr (std::floating_point<T>)
+    {
+      m_value.template emplace<float_type>(std::move(value));
+    }
+    else
+    {
+      m_value.template emplace<string_type>(std::move(value));
+    }
+  }
+};
+
+using ini_value = basic_ini_value<char>;
+using wini_value = basic_ini_value<wchar_t>;
+
+}  // namespace init
+
+#endif  // INIT_INI_VALUE_HPP
---
+++ test/unit-tests/init/ini_value_test.cpp
@@ -0,0 +1,384 @@
+#include "ini_value.hpp"
+
+#include <gtest/gtest.h>
+
+#include <sstream>  // ostringstream
+
+TEST(IniValue, Defaults)
+{
+  const init::ini_value value;
+  ASSERT_TRUE(value.is_string());
+}
+
+TEST(IniValue, DirectInitialization)
+{
+  const init::ini_value str{"foo"};
+  ASSERT_TRUE(str.is_string());
+  ASSERT_EQ("foo", str);
+}
+
+TEST(IniValue, StringValue)
+{
+  const init::ini_value value = "bar";
+  ASSERT_TRUE(value.is_string());
+  ASSERT_EQ("bar", value);
+}
+
+TEST(IniValue, WideStringValue)
+{
+  const init::wini_value value = L"abc";
+  ASSERT_TRUE(value.is_string());
+  ASSERT_EQ(L"abc", value);
+}
+
+TEST(IniValue, SignedPositiveIntegerValue)
+{
+  const init::ini_value value = 123'456;
+  ASSERT_TRUE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_EQ(123'456, value);
+}
+
+TEST(IniValue, SignedNegativeIntegerValue)
+{
+  const init::wini_value value = -123;
+  ASSERT_TRUE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_EQ(-123, value);
+}
+
+TEST(IniValue, UnsignedIntegerValue)
+{
+  const init::ini_value value = 1'563'329u;
+  ASSERT_TRUE(value.is_uint());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_EQ(1'563'329u, value);
+}
+
+TEST(IniValue, FloatValue)
+{
+  const init::ini_value value = 45.3;
+  ASSERT_TRUE(value.is_float());
+  ASSERT_EQ(45.3, value);
+}
+
+TEST(IniValue, BooleanValue)
+{
+  const init::ini_value a{true};
+  const init::ini_value b{false};
+
+  ASSERT_TRUE(a.is_bool());
+  ASSERT_TRUE(b.is_bool());
+
+  ASSERT_EQ(true, a);
+  ASSERT_EQ(false, b);
+}
+
+TEST(IniValue, DumpString)
+{
+  const init::ini_value value{"foo"};
+
+  std::ostringstream stream;
+  value.dump(stream);
+
+  ASSERT_EQ("foo", stream.str());
+}
+
+TEST(IniValue, DumpSignedInteger)
+{
+  {
+    const init::ini_value positive{123};
+
+    std::ostringstream stream;
+    positive.dump(stream);
+
+    ASSERT_EQ("123", stream.str());
+  }
+
+  {
+    const init::ini_value negative{-42};
+
+    std::ostringstream stream;
+    negative.dump(stream);
+
+    ASSERT_EQ("-42", stream.str());
+  }
+}
+
+TEST(IniValue, DumpUnsignedInteger)
+{
+  const init::ini_value value{1'337u};
+
+  std::ostringstream stream;
+  value.dump(stream);
+
+  ASSERT_EQ("1337u", stream.str());
+}
+
+TEST(IniValue, DumpFloat)
+{
+  const init::ini_value value{56.4};
+
+  std::ostringstream stream;
+  value.dump(stream);
+
+  ASSERT_EQ("56.400000", stream.str());
+}
+
+TEST(IniValue, DumpBoolean)
+{
+  {
+    const init::ini_value value{true};
+
+    std::ostringstream stream;
+    value.dump(stream);
+
+    ASSERT_EQ("true", stream.str());
+  }
+
+  {
+    const init::ini_value value{false};
+
+    std::ostringstream stream;
+    value.dump(stream);
+
+    ASSERT_EQ("false", stream.str());
+  }
+}
+
+TEST(IniValue, GetToBool)
+{
+  const init::ini_value value{true};
+
+  int i{};
+  ASSERT_THROW(value.get_to(i), std::bad_variant_access);
+
+  bool b{};
+  value.get_to(b);
+
+  ASSERT_TRUE(b);
+}
+
+TEST(IniValue, GetToInt8)
+{
+  const init::ini_value value = 105;
+
+  bool b{};
+  ASSERT_THROW(value.get_to(b), std::bad_variant_access);
+
+  init::int8 i{};
+  value.get_to(i);
+
+  ASSERT_EQ(105, i);
+}
+
+TEST(IniValue, GetToInt16)
+{
+  const init::ini_value value = 1'234;
+
+  std::string s;
+  ASSERT_THROW(value.get_to(s), std::bad_variant_access);
+
+  init::int16 i{};
+  value.get_to(i);
+
+  ASSERT_EQ(1'234, i);
+}
+
+TEST(IniValue, GetToInt32)
+{
+  const init::ini_value value = -5'721;
+
+  init::uint8 u{};
+  ASSERT_THROW(value.get_to(u), std::bad_variant_access);
+
+  init::int32 i{};
+  value.get_to(i);
+
+  ASSERT_EQ(-5'721, i);
+}
+
+TEST(IniValue, GetToInt64)
+{
+  const init::ini_value value = 284'482;
+
+  float f{};
+  ASSERT_THROW(value.get_to(f), std::bad_variant_access);
+
+  init::int64 i{};
+  value.get_to(i);
+
+  ASSERT_EQ(284'482, i);
+}
+
+TEST(IniValue, GetToUint8)
+{
+  const init::ini_value value = 12u;
+
+  double d{};
+  ASSERT_THROW(value.get_to(d), std::bad_variant_access);
+
+  init::uint8 u{};
+  value.get_to(u);
+
+  ASSERT_EQ(12u, u);
+}
+
+TEST(IniValue, GetToUint16)
+{
+  const init::ini_value value = 468u;
+
+  bool b{};
+  ASSERT_THROW(value.get_to(b), std::bad_variant_access);
+
+  init::uint16 u{};
+  value.get_to(u);
+
+  ASSERT_EQ(468u, u);
+}
+
+TEST(IniValue, GetToUint32)
+{
+  const init::ini_value value = 1'395u;
+
+  init::int32 i{};
+  ASSERT_THROW(value.get_to(i), std::bad_variant_access);
+
+  init::uint32 u{};
+  value.get_to(u);
+
+  ASSERT_EQ(1'395u, u);
+}
+
+TEST(IniValue, GetToUint64)
+{
+  const init::ini_value value = 92'564u;
+
+  std::string s;
+  ASSERT_THROW(value.get_to(s), std::bad_variant_access);
+
+  init::uint64 u{};
+  value.get_to(u);
+
+  ASSERT_EQ(92'564u, u);
+}
+
+TEST(IniValue, GetToFloat)
+{
+  const init::ini_value value = 54.3f;
+
+  int i{};
+  ASSERT_THROW(value.get_to(i), std::bad_variant_access);
+
+  float f{};
+  value.get_to(f);
+
+  ASSERT_EQ(54.3f, f);
+}
+
+TEST(IniValue, GetToDouble)
+{
+  const init::ini_value value = 164.8;
+
+  int i{};
+  ASSERT_THROW(value.get_to(i), std::bad_variant_access);
+
+  double d{};
+  value.get_to(d);
+
+  ASSERT_EQ(164.8, d);
+}
+
+TEST(IniValue, TryAs)
+{
+  init::ini_value value;
+
+  value = "foo";
+  ASSERT_TRUE(value.try_as_string());
+  ASSERT_FALSE(value.try_as_int());
+  ASSERT_FALSE(value.try_as_uint());
+  ASSERT_FALSE(value.try_as_float());
+  ASSERT_FALSE(value.try_as_bool());
+
+  value = 123;
+  ASSERT_FALSE(value.try_as_string());
+  ASSERT_TRUE(value.try_as_int());
+  ASSERT_FALSE(value.try_as_uint());
+  ASSERT_FALSE(value.try_as_float());
+  ASSERT_FALSE(value.try_as_bool());
+
+  value = 42u;
+  ASSERT_FALSE(value.try_as_string());
+  ASSERT_FALSE(value.try_as_int());
+  ASSERT_TRUE(value.try_as_uint());
+  ASSERT_FALSE(value.try_as_float());
+  ASSERT_FALSE(value.try_as_bool());
+
+  value = 12.3;
+  ASSERT_FALSE(value.try_as_string());
+  ASSERT_FALSE(value.try_as_int());
+  ASSERT_FALSE(value.try_as_uint());
+  ASSERT_TRUE(value.try_as_float());
+  ASSERT_FALSE(value.try_as_bool());
+
+  value = true;
+  ASSERT_FALSE(value.try_as_string());
+  ASSERT_FALSE(value.try_as_int());
+  ASSERT_FALSE(value.try_as_uint());
+  ASSERT_FALSE(value.try_as_float());
+  ASSERT_TRUE(value.try_as_bool());
+}
+
+TEST(IniValue, TypeIndicators)
+{
+  init::ini_value value = "foo";
+  ASSERT_TRUE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 27;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_TRUE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 452u;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_TRUE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = 542.8f;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_TRUE(value.is_float());
+  ASSERT_FALSE(value.is_bool());
+
+  value = false;
+  ASSERT_FALSE(value.is_string());
+  ASSERT_FALSE(value.is_int());
+  ASSERT_FALSE(value.is_uint());
+  ASSERT_FALSE(value.is_float());
+  ASSERT_TRUE(value.is_bool());
+}
+
+TEST(IniValue, Comparison)
+{
+  ASSERT_EQ(init::ini_value{}, init::ini_value{});
+
+  ASSERT_EQ(init::ini_value{42}, init::ini_value{42});
+  ASSERT_NE(init::ini_value{42}, init::ini_value{42u});
+  ASSERT_NE(init::ini_value{42}, init::ini_value{123});
+
+  ASSERT_NE(init::ini_value{123u}, init::ini_value{"foo"});
+  ASSERT_EQ(init::ini_value{12.3}, init::ini_value{12.3});
+
+  ASSERT_EQ(init::ini_value{true}, init::ini_value{true});
+  ASSERT_EQ(init::ini_value{false}, init::ini_value{false});
+  ASSERT_NE(init::ini_value{true}, init::ini_value{false});
+}
