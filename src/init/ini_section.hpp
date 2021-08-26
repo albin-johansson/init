81955cbce1a76e4264db53c864b769c40d6d0c24
Albin Johansson <albinoscar99@hotmail.com>
2021-08-26 21:48:38 +0200

    Update ini_section.hpp

--- src/init/ini_section.hpp
+++ src/init/ini_section.hpp
@@ -13,6 +13,15 @@
 
 namespace init {
 
+/**
+ * \struct ini_format
+ *
+ * \brief Represents the syntax used by an ini file.
+ *
+ * \tparam Char the character type that is used.
+ *
+ * \since 0.1.0
+ */
 template <typename Char>
 struct ini_format final
 {
@@ -24,6 +33,20 @@ struct ini_format final
   char_type comment = ';';        ///< Line comment token.
 };
 
+/**
+ * \class basic_ini_section
+ *
+ * \brief Represents a section in an ini file.
+ *
+ * \tparam Char the character type that is used.
+ *
+ * \see `ini_section`
+ * \see `wini_section`
+ * \see `basic_ini`
+ * \see `basic_ini_value`
+ *
+ * \since 0.1.0
+ */
 template <typename Char>
 class basic_ini_section final
 {
@@ -50,6 +73,14 @@ class basic_ini_section final
   basic_ini_section& operator=(const basic_ini_section&) = default;
   basic_ini_section& operator=(basic_ini_section&&) noexcept = default;
 
+  /**
+   * \brief Writes the contents of the section to an output stream.
+   *
+   * \param stream the output stream that will be used.
+   * \param format the syntax that will be used.
+   *
+   * \since 0.1.0
+   */
   void dump(ostream_type& stream, const format_type& format) const
   {
     for (const auto& [key, value] : m_entries)
@@ -60,6 +91,15 @@ class basic_ini_section final
     stream << '\n';
   }
 
+  /**
+   * \brief Removes an element from the section, if it exists.
+   *
+   * \param element the name of the element that will be removed.
+   *
+   * \return `true` if an element was removed; `false` otherwise.
+   *
+   * \since 0.1.0
+   */
   auto erase(const string_view_type element) -> bool
   {
     if (const auto it = m_entries.find(element); it != m_entries.end())
@@ -73,6 +113,16 @@ class basic_ini_section final
     }
   }
 
+  /**
+   * \brief Retrieves an element from the section or default-constructs one if it doesn't
+   * exist.
+   *
+   * \param element the name of the element that will be retrieved.
+   *
+   * \return the value that was either found or created.
+   *
+   * \since 0.1.0
+   */
   auto get_or_emplace(const string_view_type element) -> elem_type&
   {
     if (const auto it = m_entries.find(element); it != m_entries.end())
@@ -91,6 +141,17 @@ class basic_ini_section final
     return get_or_emplace(element);
   }
 
+  /**
+   * \brief Returns the value associated with the specified name.
+   *
+   * \param element the name of the value to obtain.
+   *
+   * \return the ini value associated with the specified name.
+   *
+   * \throws out_of_range if the section doesn't contain the specified value.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto at(const string_view_type element) -> elem_type&
   {
     if (const auto it = m_entries.find(element); it != m_entries.end())
@@ -103,6 +164,7 @@ class basic_ini_section final
     }
   }
 
+  /// \copydoc at()
   [[nodiscard]] auto at(const string_view_type element) const -> const elem_type&
   {
     if (const auto it = m_entries.find(element); it != m_entries.end())
@@ -115,36 +177,76 @@ class basic_ini_section final
     }
   }
 
+  /**
+   * \brief Indicates whether or not the section contains a value with the specified name.
+   *
+   * \param element the name of the element to look for.
+   *
+   * \return `true` if the section contains a value with the specified name; `false`
+   * otherwise.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto contains(const string_view_type element) const -> bool
   {
     return m_entries.find(element) != m_entries.end();
   }
 
+  /**
+   * \brief Returns the amount of values present in the section.
+   *
+   * \return the amount of values in the section.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto size() const noexcept -> size_type
   {
     return m_entries.size();
   }
 
+  /**
+   * \brief Indicates whether or not the section is empty.
+   *
+   * \return `true` if the section is empty; `false` otherwise.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto empty() const noexcept -> bool
   {
     return m_entries.empty();
   }
 
+  /**
+   * \brief Returns an iterator to the beginning of the section.
+   *
+   * \return an iterator to the beginning.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto begin() noexcept -> iterator
   {
     return m_entries.begin();
   }
 
+  /// \copydoc begin()
   [[nodiscard]] auto begin() const noexcept -> const_iterator
   {
     return m_entries.begin();
   }
 
+  /**
+   * \brief Returns an iterator to the end of the section.
+   *
+   * \return an iterator to the end.
+   *
+   * \since 0.1.0
+   */
   [[nodiscard]] auto end() noexcept -> iterator
   {
     return m_entries.end();
   }
 
+  /// \copydoc end()
   [[nodiscard]] auto end() const noexcept -> const_iterator
   {
     return m_entries.end();
@@ -154,7 +256,22 @@ class basic_ini_section final
   storage_type m_entries;
 };
 
+/**
+ * \typedef ini_section
+ *
+ * \brief Alias for ini sections based on `char`.
+ *
+ * \since 0.1.0
+ */
 using ini_section = basic_ini_section<char>;
+
+/**
+ * \typedef ini_section
+ *
+ * \brief Alias for ini sections based on `wchar_t`.
+ *
+ * \since 0.1.0
+ */
 using wini_section = basic_ini_section<wchar_t>;
 
 }  // namespace init
